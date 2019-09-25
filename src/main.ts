c97d92e36de57edaf93848e88006ac184862aa59
James Fuller <jafuller88@gmail.com>
2019-09-25 22:58:38 +0200

    Updated to use master.m3u8

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ function loadVideo() {
     let video: HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
     if(Hls.isSupported()) {
         var hls = new Hls();
-        hls.loadSource("http://localhost:8081/video/stream.m3u8");
+        hls.loadSource("http://localhost:8081/video/master.m3u8");
         if (video) {
             hls.attachMedia(video);
             hls.on(Hls.Events.MANIFEST_PARSED, () => {
